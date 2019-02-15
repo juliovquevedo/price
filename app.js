9d7405255d5ec6b41d1b1e8e82b68de78d2ac0c9
Julio Quevedo <juliovquevedo@gmail.com>
2019-02-14 20:30:36 -0500

    Formatted numbers to show two decimals

--- app.js
+++ app.js
@@ -20,10 +20,11 @@ function GetDiscount() {
     var discount = regPrice - discountedPrice;
     var percentDiscount = (discount / regPrice) * 100;
     var itemPrice = discountedPrice / length;
-    document.getElementById("result").innerHTML = "Regular Price: $" + regPrice + "<br>" +
-                                                "Discounted Price $" + discountedPrice + "<br>" +
-                                                "Discount Percent: " + percentDiscount + "%<br>" +
-                                                "Price per Item: $" + itemPrice + "<br>";
+    document.getElementById("result").innerHTML = 
+                "Regular Price: $" + regPrice.toFixed(2) + "<br>" +
+                "Discounted Price $" + discountedPrice.toFixed(2) + "<br>" +
+                "Discount Percent: " + percentDiscount.toFixed(2) + "%<br>" +
+                "Price per Item: $" + itemPrice.toFixed(2) + "<br>";
 }
 
 function ClearFields () {
